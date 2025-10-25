b19193a591893eebd61daa5f085ba995fe4d1c2a
Rakshith <harshith.shetty2002@gmail.com>
2025-10-25 16:22:27 +0530

    working on a cost estimation project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-4xl text-black flex font-extrabold justify-center'>Cost Estimation Project</h1>
+    </div>
+  );
+}
+
+export default App;
